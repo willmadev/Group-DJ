7dc705a551cfb2e1041d21368077eb1a226be032
Willma <65773478+willmas-here@users.noreply.github.com>
2022-06-18 02:55:04 -0700

    Update Favicon

--- components/DetailsModule.tsx
+++ components/DetailsModule.tsx
@@ -14,7 +14,7 @@ const DetailsModule: FC<DetailsModuleProps> = ({ sessionCode }) => {
     <div className={styles.container}>
       <h3>
         Join the session by scanning the QR code below or entering the session
-        code at: <Link href="/join">{URL}/join</Link>
+        code at: <Link href="/join">{`${URL}/join`}</Link>
       </h3>
       <QRCodeSVG value={`${URL}/join/${sessionCode}`} size={150} level="L" />
       <h3>Session Code: {sessionCode}</h3>
